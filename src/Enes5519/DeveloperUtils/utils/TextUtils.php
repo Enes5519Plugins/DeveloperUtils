290b79aad159fc96dc0b8a23e435709d94eb9781
Enes Yıldırım <enes5519@gmail.com>
2018-05-05 16:10:21 +0200

    Update copyright

--- src/Enes5519/DeveloperUtils/utils/TextUtils.php
+++ src/Enes5519/DeveloperUtils/utils/TextUtils.php
@@ -1,18 +1,29 @@
 <?php
 
 /*
- *               _ _
- *         /\   | | |
- *        /  \  | | |_ __ _ _   _
- *       / /\ \ | | __/ _` | | | |
- *      / ____ \| | || (_| | |_| |
- *     /_/    \_|_|\__\__,_|\__, |
- *                           __/ |
- *                          |___/
+ *   _____             _    _ _   _ _
+ *  |  __ \           | |  | | | (_) |
+ *  | |  | | _____   _| |  | | |_ _| |___
+ *  | |  | |/ _ \ \ / / |  | | __| | / __|
+ *  | |__| |  __/\ V /| |__| | |_| | \__ \
+ *  |_____/ \___| \_/  \____/ \__|_|_|___/
  *
- * @author TuranicTeam
- * @link https://github.com/TuranicTeam/Altay
  *
+ * @author Enes5519
+ * @link https://github.com/Enes5519
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 declare(strict_types=1);
